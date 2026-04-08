26a4243c0f49f7edaf48ebdb85518db62eb583f5
frolov-maks <ffrolov.maks@gmail.com>
2026-04-08 13:59:20 +0300

    Convert miles to kilometers functionality added

--- src/app/Converter.java
+++ src/app/Converter.java
@@ -1,7 +1,18 @@
 package app;
 
 public class Converter {
+
+    private static final double CONV_KM = 1.60934;
+
     public static void converter (String[] args) {
         System.out.println("Converter app v. 2.0");
+
+        double miles = 10;
+        double km = convMilesToKm(miles);
+        System.out.println("Result is " + km + " kilometers");
+    }
+
+    private static double convMilesToKm(double miles) {
+        return miles * CONV_KM;
     }
 }
